e497f77ca3244bee3134f07d1c9a42df87f04457
profLewis <dr.p.e.lewis@gmail.com>
2020-07-06 10:22:35 +0100

    clean bin
    clean bin

--- bin/local_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export HERE=/Users/plewis/librat/docs/source
-export BPMS=${BPMS-${HERE%librat*}librat}
-export BPMSROOT=$BPMS/obj
-mkdir -p $BPMSROOT
-export INIT=$BPMS/bin/examples_init.sh
